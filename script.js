078afdcea7e45b7587917693eac1c22b858aee24
DenVIII <Denis226@gmail.com>
2023-11-27 12:29:30 +0300

    Add operate function
    operate takes firstNum, secondNum, operator and then
    returns value from one of the basic math functions based
    on operator argument

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let firstNum = 2;
+let secondNum = 3;
+let operator = '/';
+
+console.log(add(4, 4));
+console.log(subtract(2, 2));
+console.log(multiply(4, 4));
+console.log(divide(2, 2));
+
+console.log(operate(firstNum, secondNum, operator))
+
+function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum)
+        case '-':
+            return subtract(firstNum, secondNum)
+        case '*':
+            return multiply(firstNum, secondNum)
+        case '/':
+            return divide(firstNum, secondNum)
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -13,8 +37,3 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
-
-console.log(add(4, 4));
-console.log(subtract(2, 2));
-console.log(multiply(4, 4));
-console.log(divide(2, 2));
