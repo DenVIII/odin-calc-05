510955c15bc0ecb014069a8b1b40955bade5a4c5
DenVIII <Denis226@gmail.com>
2023-11-29 14:06:07 +0300

    Add updateDisplay function
    Currently updateDisplay works only with simple arithmetic
    operators, doesn't support equal sign
    
    Changed name of the variables firstNum, secondNum to
    currNum, prevNum
    
    Added mask-value so result of previous calculations is
    visible, but doesn't affect the following

--- script.js
+++ script.js
@@ -1,42 +1,82 @@
-let firstNum = 2;
-let secondNum = 3;
-let operator = '/';
-
-console.log(add(4, 4));
-console.log(subtract(2, 2));
-console.log(multiply(4, 4));
-console.log(divide(2, 2));
-
-console.log(operate(operator, firstNum, secondNum))
+let prevNum = null;
+let currNum = null;
+let operator = null;
 
 const currValDisplay = document.querySelector('.curr-value');
 const prevValDisplay = document.querySelector('.prev-value');
+const maskValDisplay = document.querySelector('.mask-value');
 const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
 
 numberBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
+        maskValDisplay.textContent = '';
         currValDisplay.textContent += btn.textContent;
+        currNum = parseFloat(currValDisplay.textContent);
     })
 });
 
-function operate(operator, firstNum, secondNum) {
+operatorBtns.forEach((btn) => {
+    btn.addEventListener('click', updateDisplay)
+})
+
+function updateDisplay() {
+    if (prevNum === null) {
+        prevValDisplay.textContent = currValDisplay.textContent + ' ' + this.textContent;
+        maskValDisplay.textContent = currValDisplay.textContent;
+        currValDisplay.textContent = '';
+
+        prevNum = currNum;
+        currNum = null;
+        operator = this.textContent;
+    } else if (currNum !== null) {
+        prevValDisplay.textContent = operate(operator, prevNum, currNum) + ' ' + this.textContent;
+        maskValDisplay.textContent = currValDisplay.textContent;
+        currValDisplay.textContent = '';
+        currNum = null;
+
+        prevNum = parseFloat(prevValDisplay.textContent);
+        operator = this.textContent;
+    } else {
+        prevValDisplay.textContent = operate(operator, prevNum, currNum) + ' ' + this.textContent;
+        operator = this.textContent;
+    }
+}
+
+/* function updateDisplay() {
+    if (prevNum === null && currNum === null) {
+        prevValDisplay.textContent = currValDisplay.textContent + ' ' + this.textContent;
+        prevNum = parseFloat(currValDisplay.textContent);
+        operator = this.textContent;
+        currValDisplay.textContent = '';
+    } else {
+        currNum = parseFloat(currValDisplay.textContent)
+        currValDisplay.textContent = operate(operator, prevNum, currNum);
+        prevValDisplay.textContent = currValDisplay.textContent + this.textContent;
+        prevNum = currNum = parseFloat(prevValDisplay.textContent);
+        currValDisplay.textContent = '';
+    }
+} */
+
+function operate(operator, prevNum, currNum) {
+    console.log(operator)
     switch (operator) {
         case '+':
-            return add(firstNum, secondNum)
-        case '-':
-            return subtract(firstNum, secondNum)
-        case '*':
-            return multiply(firstNum, secondNum)
-        case '/':
-            return divide(firstNum, secondNum)
+            return add(prevNum, currNum)
+        case '−':
+            return subtract(prevNum, currNum)
+        case '×':
+            return multiply(prevNum, currNum)
+        case '÷':
+            return divide(prevNum, currNum)
         case '^':
-            return Math.pow(firstNum, secondNum)
-        case '&#8730;':
-            return Math.sqrt(firstNum)
+            return Math.pow(prevNum, currNum)
+        case '√':
+            return Math.sqrt(prevNum)
         case '1/x':
-            return inverse(firstNum)
-        case '&plusmn;':
-            return negate(firstNum)
+            return inverse(prevNum)
+        case '±':
+            return negate(prevNum)
     }
 }
 
@@ -64,19 +104,19 @@ function negate(number) {
     return -1 * number;
 }
 
-/* function percentage(operator, firstNum, secondNum) {
-    if (!secondNum) {
+/* function percentage(operator, prevNum, currNum) {
+    if (!currNum) {
         return 0
     }
     
     switch (operator) {
         case '+':
-            return add(firstNum, secondNum / firstNum * 100)
+            return add(prevNum, currNum / prevNum * 100)
         case '-':
-            return subtract(firstNum, secondNum / firstNum * 100)
+            return subtract(prevNum, currNum / prevNum * 100)
         case '*':
-            return multiply(firstNum, secondNum / 100)
+            return multiply(prevNum, currNum / 100)
         case '/':
-            return divide(firstNum, secondNum / 100)
+            return divide(prevNum, currNum / 100)
     }
 } */
